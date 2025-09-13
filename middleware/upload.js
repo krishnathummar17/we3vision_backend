fe9a5e44740db89faefa130dfc1bd9bd897225d5
krishnathummar17 <kthummar466@gmail.com>
2025-09-13 14:31:17 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,13 +4,16 @@ const path = require('path');
 // Configure storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    // Save directly into Hostinger's public_html/uploads folder
+    cb(null, path.join(__dirname, '../uploads'));
+
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
   }
 });
+
 
 // File filter
 const fileFilter = (req, file, cb) => {
