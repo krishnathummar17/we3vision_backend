855d2199b3e796e1bed2c00f124cadafa0074cb2
krishnathummar17 <kthummar466@gmail.com>
2025-10-31 10:01:50 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -42,7 +42,8 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB limit
+    fileSize: 5 * 1024 * 1024, // 5MB limit,
+    fieldSize: 10 * 1024 * 1024 // 10MB limit
   },
   fileFilter: fileFilter
 });
